edd8bc0778ea14af460a43802b42d4722309cee5
Samarth061 <140432072+Samarth061@users.noreply.github.com>
2024-08-31 23:09:46 -0500

    Edit all three files
    Add div class in html, shift container class to
    game
    Add button in game.js to change number of grids
    Restructure game.js logic

--- game.js
+++ game.js
@@ -1,26 +1,54 @@
-const container = document.querySelector(".container");
+const topbutton = document.querySelector(".topbutton");
+
+let gridButton = document.createElement('button');
+gridButton.textContent = "Change grid size";
+gridButton.style.background = 'bisque';
+gridButton.style.color = "green";
+gridButton.style.fontSize = "15px";
+gridButton.style.fontWeight = "bold";
+gridButton.style.border = '5px solid red';
+topbutton.appendChild(gridButton);
 
 function createGrid(row,column){
+
+    let container = document.createElement("div");
+    container.className = "container";
+
+    let itemheight = (100/row) + '%';
+    let itemwidth = (100/column) + '%';
+
     for(let i = 0; i < row*column; i++)
     {
         let gridItem = document.createElement('div');
         gridItem.className ='grid-item';
 
-        //gridItem.textContent = i+1;
-
         gridItem.addEventListener('mouseenter',() => {
             gridItem.classList.add('hovered');
         })
 
-        // gridItem.addEventListener('mouseleave',() => {
-        //     gridItem.classList.remove('hovered');
-        // })
+        gridItem.style.height = itemheight;
+        gridItem.style.width = itemwidth;
 
         container.appendChild(gridItem);
     }
-
+    document.body.appendChild(container);
 }
 
+function buttonClick(){
+    gridButton.addEventListener('click', () =>{
+        let oldContainer = document.querySelector('.container');
+        if (oldContainer) {
+            oldContainer.remove();
+        }
+        let gridInput = parseInt(prompt("Enter number of rows and columns you want"));
 
+        if(Number.isInteger(gridInput) && (gridInput > 0 && gridInput <= 100))
+        {
+            createGrid(gridInput,gridInput);
+        }
+        else{alert("Enter a number less than 100")};
+    })
+}
 
+buttonClick();
 createGrid(16,16);
